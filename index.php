0ae552c3565aea80b7361658665eca6210a96dc5
navikatm <l200190043@student.ums.ac.id>
2022-06-20 00:42:36 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style1.css">
+<style>
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+</style>
+</head>
+<body>
+<div class="wrapper">
+<center><p><b><font size="17">METODE DEPRESIASI</font></b></p></center></br></br></br>
+            <center><a href="straight_line.php"><font size="5">Straight Line</font></a></br></br>
+            <a href="reducing_balance.php"><font size="5">Reducing Balance</font></a></br></br>
+            <a href="sum_of_the_year.php"><font size="5">Sum of The Year</font></a></br></br>
+            <a href="unit_of_activity.php"><font size="5">Unit of Activity</font></a></br></br></center>
+</div>
+</body>
+</html>
